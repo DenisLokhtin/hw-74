e5b67c5058c8d597cc4b38b5085c2106de6d9437
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-10-02 18:38:38 +0600

    структура проекта создана

---
+++ app/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require("fs");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('List of products will be here');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('A single product by id will be here');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('Will create new product here');
+});
+
+module.exports = router;
